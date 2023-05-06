81a4bf490ecec466c2bd7c91891581fa1bf65888
Esref Burak Kaya <ebkaya.0637@gmail.com>
2023-05-06 17:25:55 +0300

    initial commit

---
+++ quinoaMarketApp/src/main/java/com/graduationproject/quinoamarketapp/QuinoaMarketAppApplication.java
@@ -0,0 +1,13 @@
+package com.graduationproject.quinoamarketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuinoaMarketAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuinoaMarketAppApplication.class, args);
+    }
+
+}
---
+++ quinoaMarketApp/src/test/java/com/graduationproject/quinoamarketapp/QuinoaMarketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graduationproject.quinoamarketapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuinoaMarketAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
